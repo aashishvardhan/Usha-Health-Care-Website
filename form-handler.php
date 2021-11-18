eb63d6c793e7de3522b537d639680c2147482542
Aashish Vardhan <79566248+aashishvardhan@users.noreply.github.com>
2021-11-18 12:46:10 +0530

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -13,7 +13,7 @@ $email_body = "User Name: $name.\n".
                 "Subject: $servicetype.\n".
                 "User Message: $message.\n";
 
-$to = 'iamprofessoryt@gmail.com';
+$to = 'ouremail@gmail.com';
 
 $headers = "From: $email_from \r\n";
 
